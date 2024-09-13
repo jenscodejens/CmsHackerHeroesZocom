869c24bd4c53ed5b7f00f9f0918cfaa721a51914
jenscodejens <161465689+jenscodejens@users.noreply.github.com>
2024-09-13 14:24:33 +0200

    Added: Dual-ConnectionString (Windows and MacOS) Fixed: @using statement for Footer component had been deleted by misstake. Fixed: Footer component is now a "sticky footer".

--- CMS/Program.cs
+++ CMS/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http;
-
+using System.Runtime.InteropServices; // Import this namespace
 
 namespace CMS
 {
@@ -32,16 +32,27 @@ namespace CMS
             // add visitorCounterService 
             builder.Services.AddScoped<VisitorCounterService>();
             builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
-            //builder.Services.AddScoped<ContentService>();
 
-            builder.Services.AddAuthentication(options =>
-                {
-                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
-                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
-                })
-                .AddIdentityCookies();
+            // Detect the operating system
+            string connectionString;
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                connectionString = builder.Configuration.GetConnectionString("DefaultConnectionWindows");
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                connectionString = builder.Configuration.GetConnectionString("DefaultConnectionMacOS");
+            }
+            else
+            {
+                throw new InvalidOperationException("Unsupported OS platform.");
+            }
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("Connection string not found for the current OS.");
+            }
+
             builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
 
@@ -67,7 +78,6 @@ namespace CMS
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
                 app.UseMigrationsEndPoint();
             }
@@ -81,10 +91,100 @@ namespace CMS
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
 
-            // Add additional endpoints required by the Identity /Account Razor components.
             app.MapAdditionalIdentityEndpoints();
 
             app.Run();
         }
     }
 }
+
+//using CMS.Components;
+//using CMS.Components.Account;
+//using CMS.Data;
+//using CMS.Extensions;
+//using CMS.Services;
+//using Microsoft.AspNetCore.Components.Authorization;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using System.Net.Http;
+
+
+//namespace CMS
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var builder = WebApplication.CreateBuilder(args);
+
+//            // Add services to the container.
+//            builder.Services.AddHttpClient("API", client =>
+//            {
+//                client.BaseAddress = new Uri(builder.Configuration["ApiBaseUrl"] ?? builder.Configuration["Kestrel:Endpoints:Http:Url"] ?? "https://localhost:44340/");
+//            });
+
+//            builder.Services.AddRazorComponents()
+//                .AddInteractiveServerComponents();
+
+//            builder.Services.AddCascadingAuthenticationState();
+//            builder.Services.AddScoped<IdentityUserAccessor>();
+//            builder.Services.AddScoped<IdentityRedirectManager>();
+//            // add visitorCounterService 
+//            builder.Services.AddScoped<VisitorCounterService>();
+//            builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
+//            //builder.Services.AddScoped<ContentService>();
+
+//            builder.Services.AddAuthentication(options =>
+//                {
+//                    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+//                    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+//                })
+//                .AddIdentityCookies();
+
+//            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//            builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
+//                options.UseSqlServer(connectionString));
+
+//            builder.Services.AddQuickGridEntityFrameworkAdapter();
+//            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+//            builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//                .AddEntityFrameworkStores<ApplicationDbContext>()
+//                .AddSignInManager()
+//                .AddDefaultTokenProviders();
+
+//            builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+//            builder.Services.AddScoped<ICreateUserService, CreateUserService>();
+//            builder.Services.AddScoped<GetCurrentUserIdService>();
+
+//            var app = builder.Build();
+
+//            // Configure the HTTP request pipeline.
+//            if (app.Environment.IsDevelopment())
+//            {
+//                app.UseMigrationsEndPoint();
+//            }
+//            else
+//            {
+//                app.UseExceptionHandler("/Error");
+//                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//                app.UseHsts();
+//                app.UseMigrationsEndPoint();
+//            }
+
+//            app.SeedDataAsync();
+//            app.UseHttpsRedirection();
+
+//            app.UseStaticFiles();
+//            app.UseAntiforgery();
+
+//            app.MapRazorComponents<App>()
+//                .AddInteractiveServerRenderMode();
+
+//            // Add additional endpoints required by the Identity /Account Razor components.
+//            app.MapAdditionalIdentityEndpoints();
+
+//            app.Run();
+//        }
+//    }
+//}
