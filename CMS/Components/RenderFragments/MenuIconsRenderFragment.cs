ce2c9fc2b028b4a1a12136353c7403ecb31a2610
jenscodejens <161465689+jenscodejens@users.noreply.github.com>
2024-10-04 12:53:41 +0200

    Just testing icons and website card at the moment

--- CMS/Components/RenderFragments/MenuIconsRenderFragment.cs
+++ CMS/Components/RenderFragments/MenuIconsRenderFragment.cs
@@ -6,22 +6,22 @@ namespace CMS.Components.RenderFragments
     {
         public static RenderFragment MenuIcons => builder =>
         {
-            // Menu icons starting from left to right. 
-            builder.OpenElement(0, "i");
+            // Wbsite card icons starting from left to right. 
+            builder.OpenElement(0, "button");
             builder.AddAttribute(1, "class", "material-icons");
             builder.AddAttribute(2, "style", "font-size: 36px;");
             builder.AddAttribute(3, "title", "Add image");
             builder.AddContent(4, "image");
             builder.CloseElement();
 
-            builder.OpenElement(5, "i");
+            builder.OpenElement(5, "button");
             builder.AddAttribute(6, "class", "material-icons");
             builder.AddAttribute(7, "style", "font-size: 36px;");
             builder.AddAttribute(8, "title", "Add a list");
             builder.AddContent(9, "list");
             builder.CloseElement();
 
-            builder.OpenElement(10, "i");
+            builder.OpenElement(10, "button");
             builder.AddAttribute(11, "class", "material-icons");
             builder.AddAttribute(12, "style", "font-size: 36px;");
             builder.AddAttribute(13, "title", "Add a text");
