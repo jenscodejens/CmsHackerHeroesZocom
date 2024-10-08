a670e029384f5273629607c6a4663f699bdddb91
EddMal <107246914+EddMal@users.noreply.github.com>
2024-10-08 11:40:38 +0200

    Pause:

--- CMS/Components/Pages/WebPages/EditWebpage.razor.cs
+++ CMS/Components/Pages/WebPages/EditWebpage.razor.cs
@@ -1,47 +1,83 @@
-﻿using CMS.Entities;
-using Microsoft.AspNetCore.Components.Authorization;
+﻿//using CMS.Entities;
+//using Microsoft.AspNetCore.Components.Authorization;
+//using Microsoft.AspNetCore.Components;
+//using Microsoft.EntityFrameworkCore.Internal;
+//using Microsoft.EntityFrameworkCore;
+
+//namespace CMS.Components.Pages.WebPages
+//{
+//    public partial class EditWebPage
+//    {
+//        [Parameter] public int WebPageId { get; set; }
+
+//        private List<Content> contentList;
+//        private int VisitCount { get; set; }
+//        private string CurrentPageUrl => NavigationManager.Uri;
+//        private int WebSiteId { get; set; }
+
+//        protected override async Task OnInitializedAsync()
+//        {
+//            using var dbContext = DbContextFactory.CreateDbContext();
+
+//            contentList = await dbContext.Contents
+//                                         .Where(c => c.WebPageId == WebPageId)
+//                                         .ToListAsync();
+
+//            // Get the WebSiteId associated with this WebPage
+//            var webPage = await dbContext.WebPages.FirstOrDefaultAsync(wp => wp.WebPageId == WebPageId);
+//            if (webPage != null)
+//            {
+//                WebSiteId = webPage.WebSiteId;
+
+//                // Check if the user is logged in
+//                var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+//                var user = authState.User;
+
+//                if (!user.Identity.IsAuthenticated)
+//                {
+//                    // Only increment the visit count if the user is not logged in
+//                    await VisitorCounterService.IncrementPageVisitAsync(WebSiteId, CurrentPageUrl);
+//                }
+
+//                // Get the updated visit count for this page
+//                VisitCount = await VisitorCounterService.GetPageVisitCountAsync(WebSiteId, CurrentPageUrl);
+//            }
+//        }
+//    }
+//}
+using CMS.Data;
+using CMS.Entities;
 using Microsoft.AspNetCore.Components;
+
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore;
-
 namespace CMS.Components.Pages.WebPages
 {
-    public partial class EditWebpage
+    public partial class EditWebPage
     {
-        [Parameter] public int WebPageId { get; set; }
+        IQueryable<Content> contents = Enumerable.Empty<Content>().AsQueryable();
+        [SupplyParameterFromQuery]
+        public int? WebPageId { get; set; }
 
-        private List<Content> contentList;
-        private int VisitCount { get; set; }
-        private string CurrentPageUrl => NavigationManager.Uri;
-        private int WebSiteId { get; set; }
+        ApplicationDbContext context = default!;
 
         protected override async Task OnInitializedAsync()
         {
-            using var dbContext = DbContextFactory.CreateDbContext();
+            context = DbFactory.CreateDbContext();
 
-            contentList = await dbContext.Contents
-                                         .Where(c => c.WebPageId == WebPageId)
-                                         .ToListAsync();
-
-            // Get the WebSiteId associated with this WebPage
-            var webPage = await dbContext.WebPages.FirstOrDefaultAsync(wp => wp.WebPageId == WebPageId);
-            if (webPage != null)
+            if (WebPageId.HasValue)
             {
-                WebSiteId = webPage.WebSiteId;
-
-                // Check if the user is logged in
-                var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-                var user = authState.User;
-
-                if (!user.Identity.IsAuthenticated)
-                {
-                    // Only increment the visit count if the user is not logged in
-                    await VisitorCounterService.IncrementPageVisitAsync(WebSiteId, CurrentPageUrl);
-                }
-
-                // Get the updated visit count for this page
-                VisitCount = await VisitorCounterService.GetPageVisitCountAsync(WebSiteId, CurrentPageUrl);
+                // Fetch content filtered by WebPageId
+                contents = context.Contents.Where(c => c.WebPageId == WebPageId.Value);
+            }
+            else
+            {
+                // Fetch all content if no WebPageId is provided
+                //contents = context.Contents;
             }
         }
+
+        public async ValueTask DisposeAsync() => await context.DisposeAsync();
     }
 }
